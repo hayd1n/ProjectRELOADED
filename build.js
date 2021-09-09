f2f15320ee61d8eff0295ace154c24dd969d4553
CRT_HAO <m831718@gmail.com>
2021-09-09 08:43:27 +0800

    編寫編譯腳本

---
+++ build.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const builder = require('electron-builder');
+
+builder.build({
+
+    projectDir: path.resolve(__dirname),
+
+    win: ['nsis'],
+    config: {
+        "appId": "com.crt_hao.projectreloaded",
+        "productName": "ProjectRELOADED",
+        "directories": {
+            "output": "build/win32"
+        },
+        "win": {
+            "icon": path.resolve(__dirname, 'assets/Icon-1024.png'),
+        },
+        "mac": {
+            "icon": path.resolve(__dirname, 'assets/Icon-1024.png'),
+        }
+    },
+})
+    .then(
+        data => console.log(data),
+        err => console.error(err)
+    );
